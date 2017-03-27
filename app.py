d5a36fb72e880785ca58101ecbdb490897888fe3
Hoda <hodahelmi@gmail.com>
2017-03-27 02:27:53 +0000

    css added!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import quandl
 
 app = Flask(__name__)
 
@@ -10,5 +11,20 @@ def main():
 def index():
   return render_template('index.html')
 
+@app.route('/graph', methods=['POST'])
+def graph():
+
+    ticker = request.form['ticker']
+    processed_ticker = ticker.upper()
+    return processed_ticker
+
+
+def businessLogic(processed_ticker):
+    
+    quandl.ApiConfig.api_key = '8pUkznBCWKATjBv-NPZX'
+    data = quandl.get_table('WIKI/PRICES')
+    
+
 if __name__ == '__main__':
-  app.run(port=33507)
+  #app.run(port=33507)
+  app.run(host='0.0.0.0')
